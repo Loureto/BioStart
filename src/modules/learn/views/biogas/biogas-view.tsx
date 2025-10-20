da27c14f5cb42854a76645ddc566ca0120154c30
joao victor <loureto.tads123@gmail.com>
2025-10-20 00:15:21 -0400

    first commit

---
+++ src/modules/learn/views/biogas/biogas-view.tsx
@@ -0,0 +1,99 @@
+import { Button, Text } from "@/components";
+import { ActionButton, AppBar } from "@/components/AppBar";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Image, ScrollView, StatusBar, View } from "react-native";
+import { CompletedDialog, CompletedFlag } from "../../components";
+import { useBiogasModel } from "./biogas-model";
+
+export const BiogasView = (methods: ReturnType<typeof useBiogasModel>) => {
+  const { biogas, visible, handleGoBack, handleCompleted, handleDialog } =
+    methods;
+
+  return (
+    <View className="flex-1 bg-zinc-50">
+      <StatusBar backgroundColor="#22c55e" />
+
+      <AppBar
+        title="O que é Biogás?"
+        leftButton={
+          <ActionButton onPress={handleGoBack}>
+            <MaterialIcons name="arrow-back" size={24} color="#FFF" />
+          </ActionButton>
+        }
+      />
+
+      <ScrollView>
+        <View>
+          <CompletedFlag
+            className="absolute right-5 top-5 z-10"
+            completed={biogas}
+          />
+
+          <Image
+            source={require("@/assets/images/biogas-info.jpg")}
+            className="h-60 w-full"
+            resizeMode="cover"
+          />
+        </View>
+
+        <View className="gap-4 px-5 py-5">
+          <Text.Title>O que é o Biogás</Text.Title>
+          <View className="flex-1">
+            <View className="flex-1 flex-row items-center gap-2">
+              <MaterialIcons name="check-box" size={24} color="#22c55e" />
+              <Text.Body className="font-semibold">
+                Energia limpa e renovável:{" "}
+              </Text.Body>
+            </View>
+
+            <Text.Body>
+              O biogás é uma alternativa sustentável aos combustíveis fósseis,
+              pois é produzido a partir de resíduos orgânicos e não agride o
+              meio ambiente.
+            </Text.Body>
+          </View>
+          <View className="flex-1">
+            <View className="flex-1 flex-row items-center gap-2">
+              <MaterialIcons name="check-box" size={24} color="#22c55e" />
+              <Text.Body className="font-semibold">
+                Redução de gases do efeito estufa:{" "}
+              </Text.Body>
+            </View>
+
+            <Text.Body>
+              Ao reaproveitar resíduos que liberariam metano na natureza, o
+              biogás ajuda a diminuir a emissão de gases poluentes responsáveis
+              pelo quecimento global
+            </Text.Body>
+          </View>
+          <View className="flex-1">
+            <View className="flex-1 flex-row items-center gap-2">
+              <MaterialIcons name="check-box" size={24} color="#22c55e" />
+              <Text.Body className="font-semibold">
+                Aproveitamento de resíduos orgânicos:{" "}
+              </Text.Body>
+            </View>
+
+            <Text.Body>
+              Restos de comida, esterco e lixo orgânico deixam de ser problema e
+              se transformam em fonte de energia, reduzindo a quantidade de lixo
+              nos aterros.
+            </Text.Body>
+          </View>
+
+          <Button
+            className="mt-4"
+            label="Marcar como concluído"
+            onPress={handleDialog}
+          />
+        </View>
+      </ScrollView>
+
+      <CompletedDialog
+        visible={visible}
+        message="Parabéns! Você concluiu este item com sucesso."
+        onClose={handleCompleted}
+      />
+    </View>
+  );
+};
